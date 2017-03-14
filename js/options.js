60286549c4d1a869873412daf9e4d9f23ed9adb7
Daku <admin@codeanimu.net>
2017-03-14 16:23:37 +0000

    authorize button now works for each service + formatting

--- js/options.js
+++ js/options.js
@@ -1,5 +1,27 @@
 "use strict";
 
+$(document).on('DOMContentLoaded', restore_options);
+$('#save').click(save_options);
+
+$('#authorize').click(function(e) {
+	e.preventDefault();
+
+	chrome.storage.sync.get({
+		cloud_service       : ''
+	}, function (options) {
+		if(options['cloud_service'] === '') { alert('Please select a Cloud Service first'); return;}
+		let cloud_service = options['cloud_service'];
+
+		cloudServices[cloud_service].authorize(function() {
+			$('#authorize-container').text('Authorized');
+		});
+	});
+});
+
+$('input[name=cloud-service]:radio').change(function(){
+	check_authorization($(this).val());
+});
+
 function save_options() {
 	let cloud_service      = $('input[name=cloud-service]:checked').val(),
 	    interval           = parseInt($('#interval').val()),
@@ -27,31 +49,38 @@ function save_options() {
 function restore_options() {
 	chrome.storage.sync.get({
 		cloud_service       : '',
+		dropbox_token       : '',
+		googledrive_token   : '',
+		onedrive_token      : '',
+
 		interval            : 60,
 		encryption          : 'text',
 		use_unix_timestamp  : false
 	}, function (options) {
 		$('input[name=cloud-service][value='+options['cloud_service']+']').prop('checked', true);
+		check_authorization(options['cloud_service']);
+
 		$('#interval').val(options['interval']);
 		$('#encryption').val(options['encryption']);
 		$('#use-unix-timestamp').prop('checked', options['use_unix_timestamp']);
 	});
 }
 
-$(document).on('DOMContentLoaded', restore_options);
-$('#save').click(save_options);
+function check_authorization(cloud_service) {
+	let token_name = cloud_service.toLowerCase()+'_token';
 
-$('#authorize').click(function(e) {
-	e.preventDefault();
-
-	chrome.storage.sync.get({
-		cloud_service       : ''
-	}, function (options) {
-		if(options['cloud_service'] === '') { alert('Please select a Cloud Service first'); return;}
-		let cloud_service = options['cloud_service'];
-
-		cloudServices[cloud_service].authorize(function() {
-			$('#authorize-container').text('Authorized');
-		});
+	let getOptions = {};
+	getOptions[token_name] = '';
+	chrome.storage.sync.get(getOptions, function (options) {
+		let current_token = options[token_name];
+		if(current_token !== '') {
+			$('#authorize-container').html('').append('Authorized | ').append(
+				$('<a/>', {href: '#', id: 'authorize', text: 'Reauthorize?'})
+			);
+		} else {
+			$('#authorize-container').html('').append(
+				$('<a/>', {href: '#', id: 'authorize', text: 'Authorize'})
+			)
+		}
 	});
-});
+}
